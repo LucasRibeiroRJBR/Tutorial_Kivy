2753a88f2e64458d8d3d9bf79cd6e13ac771ff8e
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2021-06-07 09:30:42 -0300

    Button / Labels

---
+++ Scripts/freeCodeCamp/my_codes/1_button_label.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+
+class button_label(App):  # Nome da classe = arquivo .kv
+    pass
+
+button_label().run()
