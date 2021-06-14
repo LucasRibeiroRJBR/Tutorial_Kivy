cc36f5eb4e9a896c4aff744da32a47c1072217db
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2021-06-14 17:42:14 -0300

    x

---
+++ Scripts/freeCodeCamp/my_codes/2_BOX_LAYOUTS/main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+class ExemploBoxLayout(BoxLayout):
+    pass
+
+class MainWidget(Widget):
+    pass
+
+class box_layouts(App):
+    pass
+
+box_layouts().run()
